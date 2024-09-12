14f647f3bb50151db65fe1ed5920b592af3e3101
Kamal Hakim <coperbyte.sol@gmail.com>
2024-09-12 08:46:57 -0700

    Compiles assets for Github Pages

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
-var DappToken = artifacts.require("./DappToken.sol");
-var DappTokenSale = artifacts.require("./DappTokenSale.sol");
+var DaapCoin = artifacts.require("./DaapCoin.sol");
+var DaapCoinSale = artifacts.require("./DaapCoinSale.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(DappToken, 1000000).then(function() {
+  deployer.deploy(DaapCoin, 1000000).then(function() {
     // Token price is 0.001 Ether
     var tokenPrice = 1000000000000000;
-    return deployer.deploy(DappTokenSale, DappToken.address, tokenPrice);
+    return deployer.deploy(DaapCoinSale, DaapCoin.address, tokenPrice);
   });
 };
