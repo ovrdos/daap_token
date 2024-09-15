4aee38e481c137eaa992c85516d7462bca0f3420
Kamal Hakim <coperbyte.sol@gmail.com>
2024-09-15 01:15:49 -0700

    Compiles assets for Github Pages

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,8 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  compilers: {
+    solc: {
+      version: "^0.4.24",  // Use a specific version range if you want to be flexible
+    }
+  }
 };
+
